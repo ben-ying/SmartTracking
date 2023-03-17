8ab75fda43f6a2a6287702b614f424bd3e89a692
ben <benying1988@gmail.com>
2023-03-17 12:45:44 +0800

    feat: retrieve scopes from firestore, refs #2

--- app/src/main/java/com/mobileappt20/ui/overview/OverviewScopeFragment.kt
+++ app/src/main/java/com/mobileappt20/ui/overview/OverviewScopeFragment.kt
@@ -1,14 +1,21 @@
 package com.mobileappt20.ui.overview
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.mobileappt20.R
 import com.mobileappt20.databinding.FragmentOverviewScopeBinding
+import kotlinx.coroutines.launch
 
 class OverviewScopeFragment : Fragment() {
 
@@ -16,6 +23,27 @@ class OverviewScopeFragment : Fragment() {
 
     private val binding get() = _binding!!
     private val viewModel: OverviewScopeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.retrieveScopeUiState.collect {
+                    binding.progressLayout.progressBar.isVisible = false
+                    if (it.action == Action.RETRIEVE_SCOPE_SUCCESS) {
+                        Log.d("test", "documents: ${it.documents}")
+                    } else if (it.action == Action.RETRIEVE_SCOPE_FAILED) {
+                        Toast.makeText(
+                            requireContext(),
+                            R.string.retrieve_scope_failed,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +53,12 @@ class OverviewScopeFragment : Fragment() {
         _binding = FragmentOverviewScopeBinding.inflate(inflater, container, false)
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_addScopeFragment)
+        }
+
+        val calendarView = binding.calendarLayout.calendarView
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            binding.progressLayout.progressBar.isVisible = true
+            viewModel.retrieveScope("scopes/$year/${month + 1}/$dayOfMonth/scope")
         }
 
         return binding.root
--- app/src/main/java/com/mobileappt20/ui/overview/OverviewScopeViewModel.kt
+++ app/src/main/java/com/mobileappt20/ui/overview/OverviewScopeViewModel.kt
@@ -1,13 +1,56 @@
 package com.mobileappt20.ui.overview
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+enum class Action {
+    RETRIEVE_SCOPE_SUCCESS,
+    RETRIEVE_SCOPE_FAILED,
+    NONE
+}
+
+data class RetrieveScopeUiState(
+    val action: Action = Action.NONE,
+    val documents: List<DocumentSnapshot> = listOf()
+)
 
 class OverviewScopeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is overview scope Fragment"
+    companion object {
+        const val TAG = "OverviewScopeViewModel"
     }
-    val text: LiveData<String> = _text
+
+    private val _retrieveScopeUiState = MutableStateFlow(RetrieveScopeUiState())
+    val retrieveScopeUiState: StateFlow<RetrieveScopeUiState> = _retrieveScopeUiState.asStateFlow()
+    private val firestore = Firebase.firestore
+
+    fun retrieveScope(collection: String) {
+        firestore.collection(collection)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                Log.d(TAG, "Retrieve documents: ${querySnapshot.documents}")
+                _retrieveScopeUiState.update { currentState ->
+                    currentState.copy(
+                        action = Action.RETRIEVE_SCOPE_SUCCESS,
+                        documents = querySnapshot.documents
+                    )
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error retrieve documents", e)
+                _retrieveScopeUiState.update { currentState ->
+                    currentState.copy(
+                        action = Action.RETRIEVE_SCOPE_FAILED,
+                        documents = listOf()
+                    )
+                }
+            }
+    }
 }
