efcff6ac778403af36be4fa2a035c7d32c5fd54e
ben <benying1988@gmail.com>
2023-03-17 12:20:22 +0800

    feat: create scope, refs #2, close #2

--- app/src/main/java/com/mobileappt20/ui/add/AddScopeFragment.kt
+++ app/src/main/java/com/mobileappt20/ui/add/AddScopeFragment.kt
@@ -1,21 +1,21 @@
 package com.mobileappt20.ui.add
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.mobileappt20.R
 import com.mobileappt20.databinding.FragmentAddScopeBinding
 import kotlinx.coroutines.launch
+import java.util.*
+
 
 class AddScopeFragment : Fragment() {
 
@@ -23,17 +23,39 @@ class AddScopeFragment : Fragment() {
 
     private val binding get() = _binding!!
     private var scopeIndex = 0
+    private var scopeName = String()
     private val viewModel: AddScopeViewModel by viewModels()
-    private val firestore = Firebase.firestore
+    private var year = Calendar.getInstance().get(Calendar.YEAR)
+    private var month = Calendar.getInstance().get(Calendar.MONTH) + 1
+    private var dayOfMonth = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.uiState.collect {
+                viewModel.scopeIndexUiState.collect {
                     scopeIndex = it.scopeIndex
                     displayScope()
-                    Log.d("AddScopeFragment", "scopeIndex: ${it.scopeIndex}")
+                }
+            }
+        }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.createScopeUiState.collect {
+                    binding.progressLayout.progressBar.isVisible = false
+                    if (it.action == Action.CREATE_SCOPE_SUCCESS) {
+                        Toast.makeText(
+                            requireContext(),
+                            R.string.scope_created_successfully,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else if (it.action == Action.CREATE_SCOPE_FAILED) {
+                        Toast.makeText(
+                            requireContext(),
+                            R.string.scope_created_failed,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
             }
         }
@@ -53,21 +75,20 @@ class AddScopeFragment : Fragment() {
         binding.scopeLayout.nextBtn.setOnClickListener {
             viewModel.setScopeIndex(scopeIndex, 1)
         }
+        val calendarView = binding.calendarLayout.calendarView
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            this.year = year
+            this.month = month + 1
+            this.dayOfMonth = dayOfMonth
+        }
         binding.addScopeBtn.setOnClickListener {
-            // Create a new user with a first and last name
-            val user = hashMapOf(
-                "first" to "Ada",
-                "last" to "Lovelace",
-                "born" to 1815
+            binding.progressLayout.progressBar.isVisible = true
+            viewModel.createScope(
+                scopeName,
+                Calendar.getInstance().timeInMillis,
+                calendarView.date,
+                "scopes/$year/$month/$dayOfMonth/scope"
             )
-            firestore.collection("scopes")
-                .add(user)
-                .addOnSuccessListener { documentReference ->
-                    Log.d("AddScopeFragment", "DocumentSnapshot added with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    Log.w("AddScopeFragment", "Error adding document", e)
-                }
         }
 
         return binding.root
@@ -76,22 +97,23 @@ class AddScopeFragment : Fragment() {
     private fun displayScope() {
         when (scopeIndex) {
             0 -> {
+                scopeName = getString(R.string.scope_a)
                 binding.scopeLayout.scopeImage.setImageResource(R.mipmap.scope_img)
-                binding.scopeLayout.scopeTitle.setText(R.string.scope_a)
             }
             1 -> {
+                scopeName = getString(R.string.scope_b)
                 binding.scopeLayout.scopeImage.setImageResource(R.mipmap.scope_img)
-                binding.scopeLayout.scopeTitle.setText(R.string.scope_b)
             }
             2 -> {
+                scopeName = getString(R.string.scope_c)
                 binding.scopeLayout.scopeImage.setImageResource(R.mipmap.scope_img)
-                binding.scopeLayout.scopeTitle.setText(R.string.scope_c)
             }
             3 -> {
+                scopeName = getString(R.string.scope_d)
                 binding.scopeLayout.scopeImage.setImageResource(R.mipmap.scope_img)
-                binding.scopeLayout.scopeTitle.setText(R.string.scope_d)
             }
         }
+        binding.scopeLayout.scopeTitle.text = scopeName
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/mobileappt20/ui/add/AddScopeViewModel.kt
+++ app/src/main/java/com/mobileappt20/ui/add/AddScopeViewModel.kt
@@ -1,19 +1,39 @@
 package com.mobileappt20.ui.add
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-data class ScopeUiState(
+enum class Action {
+    CREATE_SCOPE_SUCCESS,
+    CREATE_SCOPE_FAILED,
+    NONE
+}
+data class ScopeIndexUiState(
     val scopeIndex: Int = 0,
+)
+
+data class CreateScopeUiState(
+    val action: Action = Action.NONE,
+    val documentReferenceId: String? = null
 )
 
 class AddScopeViewModel : ViewModel() {
 
-    private val _uiState = MutableStateFlow(ScopeUiState())
-    val uiState: StateFlow<ScopeUiState> = _uiState.asStateFlow()
+    companion object {
+        const val TAG = "AddScopeViewModel"
+    }
+
+    private val _scopeIndexUiState = MutableStateFlow(ScopeIndexUiState())
+    val scopeIndexUiState: StateFlow<ScopeIndexUiState> = _scopeIndexUiState.asStateFlow()
+    private val _createScopeUiState = MutableStateFlow(CreateScopeUiState())
+    val createScopeUiState: StateFlow<CreateScopeUiState> = _createScopeUiState.asStateFlow()
+    private val firestore = Firebase.firestore
 
     fun setScopeIndex(index: Int, delta: Int) {
         var scopeIndex = index + delta
@@ -23,10 +43,43 @@ class AddScopeViewModel : ViewModel() {
             scopeIndex = 3
         }
 
-        _uiState.update { currentState ->
+        _scopeIndexUiState.update { currentState ->
             currentState.copy(
                 scopeIndex = scopeIndex
             )
         }
+    }
+
+    fun createScope(
+        name: String,
+        createTime: Long,
+        scheduleTime: Long,
+        collection: String
+    ) {
+        val scope = hashMapOf(
+            "name" to name,
+            "scheduleTime" to scheduleTime,
+            "createTime" to createTime,
+        )
+        firestore.collection(collection)
+            .add(scope)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "Scope added with ID: ${documentReference.id}")
+                _createScopeUiState.update { currentState ->
+                    currentState.copy(
+                        action = Action.CREATE_SCOPE_SUCCESS,
+                        documentReferenceId = documentReference.id
+                    )
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+                _createScopeUiState.update { currentState ->
+                    currentState.copy(
+                        action = Action.CREATE_SCOPE_FAILED,
+                        documentReferenceId = null
+                    )
+                }
+            }
     }
 }
